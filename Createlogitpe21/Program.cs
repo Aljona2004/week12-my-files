8778f954163df8746b2bee67f474540f66c95dd9
Opilane TTHK <opilane@tthk.ee>
2021-11-18 09:57:48 +0200

    createlogitpe21

---
+++ Createlogitpe21/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.IO;
+
+namespace Createlogitpe21
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            string newDirectory = "LogItpe21";
+
+            CreateMyDirectory(newDirectory);
+
+           
+        }
+        private static void CreateMyDirectory(string myDirectoryName)
+        {
+            string rootDirectory = @"C:\Users\opilane\samples";
+            string directoryFullPath = $@"{rootDirectory}\{myDirectoryName}";
+            if (Directory.Exists(directoryFullPath))
+            {
+                Console.WriteLine($"Directory {myDirectoryName} already exists in {rootDirectory}.");
+            }
+            else
+            {
+                Directory.CreateDirectory(directoryFullPath);
+                Console.WriteLine($"Directory {myDirectoryName} has been created.");
+            }
+        }
+
+    }
+}
---
+++ myFiles/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.IO;
+
+namespace myFiles
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CreateFilesFromMyList(ReplaceNumbers(GetDataFromFile()));
+            updateMyFiles(ReplaceNumbers(GetDataFromFile()));
+            
+        }
+
+        private static string[] GetDataFromFile()
+        {
+            string sourceFilePath = $@"C:\Users\opilane\samples\LogItpe21\myFilse.txt";
+            string[] dataFromFile = File.ReadAllLines(sourceFilePath);
+            return dataFromFile;
+        }
+
+        private string[] ReplaceNumbers(string[] sourceArray)
+        {
+            for(int i = 0; i < sourceArray.Length; i++)
+            {
+                sourceArray[i] = sourceArray[i].Replace('4', 'a').Replace('3', 'e').Replace('0', 'o').Replace('1', 'i');
+
+            }
+            return sourceArray;
+        }
+
+        private static void CreateFilesFromMyList(string[] arrayOfNames)
+        {
+            string rootPath = $@"C:\Users\opilane\samples\LogItpe21";
+
+            for(int i = 0; i < arrayOfNames.Length; i++)
+            {
+                File.Create($@"{rootPath}\{arrayOfNames[i]}.txt");
+            }
+        }
+        private static void updateMyFiles(string[] updateArray)
+        {
+            string sourceFilePath = $@"C:\Users\opilane\samples\LogItpe21\myFilse.txt";
+            File.WriteAllLines(sourceFilePath, updateArray);
+        }
+    }
+}
